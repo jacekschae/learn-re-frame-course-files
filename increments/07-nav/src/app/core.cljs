06bc2fb476af0886c3aa27a05fd2b34fdc1827f0
Jacek Schae <jacek.schae@gmail.com>
2019-02-07 08:30:03 +0100

    07-nav

---
+++ increments/07-nav/src/app/core.cljs
@@ -0,0 +1,23 @@
+(ns app.core
+  (:require [reagent.core :as r]
+            [re-frame.core :as rf]
+            [app.db]
+            [app.theme :refer [cheffy-theme]]
+            ["@smooth-ui/core-sc" :refer [Normalize ThemeProvider Button]]))
+
+(defn app
+  []
+  [:<>
+   [:> Normalize]
+   [:> ThemeProvider {:theme cheffy-theme}
+    [:> Button "hello"]]])
+
+(defn ^:dev/after-load start
+  []
+  (rf/dispatch-sync [:initialize-db])
+  (r/render [app]
+            (.getElementById js/document "app")))
+
+(defn ^:export init
+  []
+  (start))
