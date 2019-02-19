d3d8b5ba8666f59745c846ba5edb49078c675e5a
Jacek Schae <jacek.schae@gmail.com>
2019-02-19 21:12:46 +0100

    20-reg-cofx

---
+++ increments/20-reg-cofx/src/app/auth/events.cljs
@@ -0,0 +1,79 @@
+(ns app.auth.events
+  (:require [re-frame.core :refer [reg-event-fx reg-event-db after]]))
+
+(def cheffy-user-key "cheffy-user")
+
+(defn set-user-ls!
+  [{:keys [auth]}]
+  (when (:uid auth) (.setItem js/localStorage cheffy-user-key (str auth))))
+
+(defn remove-user-ls!
+  []
+  (.removeItem js/localStorage cheffy-user-key))
+
+(def set-user-interceptors [(after set-user-ls!)])
+(def remove-user-interceptors [(after remove-user-ls!)])
+
+(reg-event-db
+ :load-user-ls!
+ (fn [db _]
+   (let [auth (js->clj (.getItem js/localStorage cheffy-user-key))]
+     (assoc-in db [:auth] auth))))
+
+(reg-event-fx
+ :sign-up
+ set-user-interceptors
+ (fn [{:keys [db]} [_ {:keys [first-name last-name email password]}]]
+   {:db (-> db
+            (assoc-in [:auth :uid] email)
+            (assoc-in [:users email] {:id email
+                                      :profile {:first-name first-name
+                                                :last-name last-name
+                                                :email email
+                                                :password password
+                                                :img "img/avatar.jpg"}
+                                      :saved #{}
+                                      :inboxes {}}))
+    :dispatch [:set-active-nav :saved]}))
+
+(reg-event-fx
+ :log-in
+ set-user-interceptors
+ (fn [{:keys [db]} [_ {:keys [email password]}]]
+   (let [user (get-in db [:users email])
+         correct-password? (= (get-in user [:profile :password]) password)]
+     (cond
+       (not user)
+       {:db (assoc-in db [:errors :email] "User not found")}
+
+       (not correct-password?)
+       {:db (assoc-in db [:errors :email] "Wrong password")}
+
+       correct-password?
+       {:db (-> db
+                (assoc-in [:auth :uid] email)
+                (update-in [:errors] dissoc :email))
+        :dispatch [:set-active-nav :saved]}))))
+
+(reg-event-fx
+ :log-out
+ remove-user-interceptors
+ (fn [{:keys [db]} _]
+   {:db (assoc-in db [:auth :uid] nil)
+    :dispatch [:set-active-nav :recipes]}))
+
+(reg-event-db
+ :update-profile
+ (fn [db [_ profile]]
+   (let [uid (get-in db [:auth :uid])]
+     (update-in db [:users uid :profile] merge (select-keys profile [:first-name :last-name])))))
+
+(reg-event-fx
+ :delete-account
+ remove-user-interceptors
+ (fn [{:keys [db]} _]
+   (let [uid (get-in db [:auth :uid])]
+     {:db (-> db
+              (assoc-in [:auth :uid] nil)
+              (update-in [:users] dissoc uid))
+      :dispatch [:set-active-nav :recipes]})))
---
+++ increments/20-reg-cofx/src/app/nav/views/authenticated.cljs
@@ -0,0 +1,38 @@
+(ns app.nav.views.authenticated
+  (:require [re-frame.core :as rf]
+            [app.nav.views.nav-item :refer [nav-item]]
+            ["@smooth-ui/core-sc" :refer [Box]]))
+
+(defn authenticated
+  []
+  (let [active-nav @(rf/subscribe [:active-nav])
+        nav-items [{:id :saved
+                    :name "Saved"
+                    :href "#saved"
+                    :dispatch #(rf/dispatch [:set-active-nav :saved])}
+                   {:id :recipes
+                    :name "Recipes"
+                    :href "#recipes"
+                    :dispatch #(rf/dispatch [:set-active-nav :recipes])}
+                   {:id :inboxes
+                    :name "Inbox"
+                    :href "#inbox"
+                    :dispatch #(rf/dispatch [:set-active-nav :inboxes])}
+                   {:id :become-a-chef
+                    :name "Chef"
+                    :href "#become-a-chef"
+                    :dispatch #(rf/dispatch [:set-active-nav :become-a-chef])}
+                   {:id :profile
+                    :name "Profile"
+                    :href "#profile"
+                    :dispatch #(rf/dispatch [:set-active-nav :profile])}]]
+    [:> Box {:display "flex"
+             :justify-content "flex-end"
+             :py 1}
+     (for [{:keys [id name href dispatch]} nav-items]
+       [nav-item {:key id
+                  :id id
+                  :name name
+                  :href href
+                  :dispatch dispatch
+                  :active-nav active-nav}])]))
