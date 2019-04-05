8def9e9d50319253b30a047f6c895ca34a9f8288
Jacek Schae <jacek.schae@gmail.com>
2019-04-05 14:46:23 +0200

    47-inbox-page-events

---
+++ increments/47-inbox-page-events/src/app/components/form_group.cljs
@@ -0,0 +1,19 @@
+(ns app.components.form-group
+  (:require ["@smooth-ui/core-sc" :refer [FormGroup Label Input Textarea]]))
+
+(defn form-group
+  [{:keys [id label type values textarea]}]
+  [:> FormGroup
+   [:> Label {:html-for id} label]
+   (if textarea
+     [:> Textarea {:control true
+                   :rows 6
+                   :id id
+                   :type type
+                   :value (id @values)
+                   :on-change #(swap! values assoc id (.. % -target -value))}]
+     [:> Input {:control true
+                :id id
+                :type type
+                :value (id @values)
+                :on-change #(swap! values assoc id (.. % -target -value))}])])
